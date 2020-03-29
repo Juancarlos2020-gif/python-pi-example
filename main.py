2a270614b59ccebfb29a866e289ae5d403ad5cee
Juancarlos2020-git <apodo1234@gmail.com>
2020-03-29 20:03:58 +0200

    Cambios en main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 #!user/bin/python3.6
 def main (msg):
-<<<<<<< HEAD
 // 2º comentario en main.py de pi
-=======
 // comentario
->>>>>>> refs/remotes/origin/master
     print (msg)
 main("Hola gente")
